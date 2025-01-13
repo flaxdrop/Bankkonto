80e489559fb3fdaf0e43537343cf7cf3d7b68dc7
BillyBobNilsson <143813616+BillybobNilsson@users.noreply.github.com>
2025-01-13 11:37:00 +0100

    Pre-compile test
    Co-authored-by: Viktorkvick <viktor.kvick@chasacademy.se>
    Co-authored-by: John Collinder <flaxdrop@users.noreply.github.com>
    Co-authored-by: erikdsp <erikdsp@users.noreply.github.com>

--- Bank.cpp
+++ Bank.cpp
@@ -1,12 +1,52 @@
 #include "Bank.h"
 #include <iostream>
 
-void Bank::createAccount(int accountNumber)
+
+void Bank::createAccount(int accountNumber, int balance)
 {
-    
+    std::lock_guard<std::mutex> lock_accounts(allAccountsMutex);
+    if (m_accounts.count(accountNumber) != 0)
+    {
+        // error message: account already exists
+        return;
+    }
+
+    m_accounts.emplace(accountNumber, BankAccount(accountNumber));
+    // lockguard allAccountsMutex
+    // check if account already exists
 }
 
-void Bank::printBalance()
+bool Bank::accountExists(int accountNumber)
 {
-    
+    return (m_accounts.count(accountNumber) != 0);
+}
+
+void Bank::printBalance() const
+{
+    std::cout << "Saldo" << std::endl;
+    for (const auto &account : m_accounts)
+    {
+        std::cout << "kontonummer: " << account.first << " saldo: " << account.second.getBalance() << std::endl;
+    }
+}
+
+BankAccount *Bank::getAccount(int accountNumber)
+{
+    if (auto search = m_accounts.find(accountNumber); search != m_accounts.end())
+        return &search->second;
+    else
+    {
+        std::cout << "Account does not exist" << std::endl;
+        return nullptr;
+    }
+}
+
+std::vector<int> Bank::getAccountNumbers()
+{
+    std::vector<int> allAccountNumbers;
+    for (auto &account : m_accounts)
+    {
+        allAccountNumbers.push_back(account.first);
+    }
+    return allAccountNumbers;
 }
