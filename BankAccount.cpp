84026633c244947726d2c5b84d4c0a097577a8d9
BillybobNilsson <david.reyes@chasacademy.se>
2025-01-20 11:05:01 +0100

    Added transaction delays

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -5,23 +5,35 @@ BankAccount::BankAccount(int accountnumber, int balance)
 
 int BankAccount::getBalance() const
 {
-    std::lock_guard<std::mutex> lock(m_accountMutex);
+    std::shared_lock<std::shared_mutex> lock(m_accountMutex);
+
+    int randomValue{Random::get_random(1, 3)};
+    std::this_thread::sleep_for(std::chrono::seconds(randomValue));
     return m_balance;
 }
 
 void BankAccount::deposit(int amount)
 {
-    std::lock_guard<std::mutex> lock(m_accountMutex);
+    std::lock_guard<std::shared_mutex> lock(m_accountMutex);
+    
+    int randomValue{Random::get_random(1, 3)};
+    std::this_thread::sleep_for(std::chrono::seconds(randomValue));
     m_balance += amount;
 }
 
 void BankAccount::withdraw(int amount)
 {
-    std::lock_guard<std::mutex> lock(m_accountMutex);
-
+    std::lock_guard<std::shared_mutex> lock(m_accountMutex);
+    
+    int randomValue{Random::get_random(1, 3)};
+    std::this_thread::sleep_for(std::chrono::seconds(randomValue));
     m_balance -= amount;
 }
 int BankAccount::getAccountNumber()
 {
+    std::shared_lock<std::shared_mutex> lock(m_accountMutex);
+    
+    int randomValue{Random::get_random(1, 3)};
+    std::this_thread::sleep_for(std::chrono::seconds(randomValue));
     return m_accountNumber;
 }
