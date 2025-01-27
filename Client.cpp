dca25f36a25169eefbf7bd5c0991e9a807c4ec9a
Erik Dahl <erik@iunderlandet.se>
2025-01-27 09:58:41 +0100

    add reports vector in main

--- Client.cpp
+++ Client.cpp
@@ -1,12 +1,12 @@
 #include "Client.h"
 #include <sstream>
 
-Client::Client(const std::string &name, Bank &bank_ref) : name(name), bank(bank_ref)
+Client::Client(const std::string &name, Bank &bank_ref, std::vector<std::string>& reports, std::mutex& report_mutex) : name(name), bank(bank_ref)
 {
 }
-void Client::client(Bank &bank_ref, const std::string &name)
+void Client::client(Bank &bank_ref, const std::string &name, std::vector<std::string>& reports, std::mutex& report_mutex)
 {
-    Client client(name, bank_ref);
+    Client client(name, bank_ref, reports, report_mutex);
     std::ostringstream stream;
     std::shared_ptr<BankAccount> account_ref;
     {
@@ -22,8 +22,9 @@ void Client::client(Bank &bank_ref, const std::string &name)
         account_ref = bank_ref.getAccount(accountNumber);
         if (account_ref == nullptr)
         {
-            stream << "Account does not exist" << std::endl;
-            std::cout << stream.str();
+            stream << "Transaction attempt " << std::chrono::system_clock::now() << " Account does not exist" << std::endl;
+            std::lock_guard<std::mutex> report_lock(report_mutex);
+            reports.emplace_back(stream.str());
             return;
         }
     }
@@ -34,8 +35,11 @@ void Client::client(Bank &bank_ref, const std::string &name)
         {
             int randomAmount{Random::get_random(1, 100)};
             account_ref->deposit(randomAmount);
-            stream << client.name << "deposited " << randomAmount << " kr into account " << account_ref->getAccountNumber() << "." << std::endl;
-            std::cout << stream.str();
+            stream << client.name << "deposited " << randomAmount << " kr into account " << account_ref->getAccountNumber() 
+                   << ", " << std::chrono::system_clock::now() << std::endl;
+            std::lock_guard<std::mutex> report_lock(report_mutex);
+            reports.emplace_back(stream.str());
+            // Printing is now handled by Report thread
         }
         else if (randomValue == 1)
         {
@@ -43,19 +47,24 @@ void Client::client(Bank &bank_ref, const std::string &name)
             if (account_ref->getBalance() >= randomAmount)
             {
                 account_ref->withdraw(randomAmount);
-                stream << client.name << "withdrew " << randomAmount << " kr from account " << account_ref->getAccountNumber() << "." << std::endl;
-                std::cout << stream.str();
+                stream << client.name << "withdrew " << randomAmount << " kr from account " << account_ref->getAccountNumber() 
+                       << ", " << std::chrono::system_clock::now() << std::endl;
+                std::lock_guard<std::mutex> report_lock(report_mutex);
+                reports.emplace_back(stream.str());
             }
             else
             {
-                stream << "Insufficient funds" << std::endl;
-                std::cout << stream.str();
+                stream << "Transaction attempt " << std::chrono::system_clock::now() << "Insufficient funds" << std::endl;
+                std::lock_guard<std::mutex> report_lock(report_mutex);
+                reports.emplace_back(stream.str());
             }
         }
         else
         {
-            stream << client.name << "checked balance in account: " << account_ref->getAccountNumber() << ". Balance: " << account_ref->getBalance() << ".\n";
-            std::cout << stream.str();
+            stream << client.name << "checked balance in account: " << account_ref->getAccountNumber() << ". Balance: " << account_ref->getBalance() 
+                   << std::chrono::system_clock::now() << std::endl;
+            std::lock_guard<std::mutex> report_lock(report_mutex);
+            reports.emplace_back(stream.str());
         }
     }
 
