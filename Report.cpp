99edcd95eada75d7aac9383e7d5cd76a2c88ebc2
erikdsp <166148828+erikdsp@users.noreply.github.com>
2025-02-10 12:13:54 +0100

    revert Report.cpp

--- Report.cpp
+++ Report.cpp
@@ -1,25 +1,22 @@
 #include "Report.h"
-#include <sstream>
-Report::Report(std::vector<std::string>& reports, std::mutex& report_mutex, 
-           std::condition_variable& cv, std::atomic_bool &reports_left_to_do, const std::string& filename){}
 
 
 //! @brief Worker thread for logging reports to file and stdout
-//! Loops while main thread says there's reports left to do and waits on a
-//! condition variable signal that there's avaiable work.
-//! When work is performed, loops though available reports and prints to file 
-//! and stdout.
+//! Loops as long as reports_left_to_do is true
+//! When all client threads are done main() will set reports_left_to_do to false
+//! and the loop exits 
+//! Waits on a condition variable. When it signals avaiable work.
+//! it loops though available report and prints to file and stdout.
 //! Then the vector containing ingoing reports are cleared.
-
 void Report::report(std::vector<std::string>& reports, std::mutex& report_mutex, 
-                    std::condition_variable& cv, std::atomic_bool &reports_left_to_do, const std::string& filename)                    
+                    std::condition_variable& report_available, std::atomic_bool &reports_left_to_do, const std::string& filename)                    
 {
     std::ofstream report_file(filename);
 
     while (reports_left_to_do)
     {
         std::unique_lock report_lock(report_mutex);
-        cv.wait(report_lock);
+        report_available.wait(report_lock);
 
         // print reports to file and stdout
         for (auto& report : reports) {
@@ -33,12 +30,13 @@ void Report::report(std::vector<std::string>& reports, std::mutex& report_mutex,
 
 }
 
-
+//! checks if file exists in current directory
 bool file_exists(const std::string& filename) {
     std::ifstream file(filename);
     return file.good();
 }
 
+//! get current date in format suitable for a filename
 std::string get_current_date_cstyle() {
     std::time_t t = std::time(nullptr);
     char time_string[100]; 
@@ -48,6 +46,7 @@ std::string get_current_date_cstyle() {
     else return "date_error";
 }
 
+//! Function generates a filename based on current date
 std::string generate_filename() {
     const std::string namebase { "Transactions-" + get_current_date_cstyle() };
     std::string name_to_check { namebase + ".txt" };
