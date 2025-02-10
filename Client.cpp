d91a4d725dbfa298b05cc47b1754d8bd379f0c96
DavidReyes <143813616+DavidJanReyes@users.noreply.github.com>
2025-02-10 12:10:32 +0100

    Edited Client files

--- Client.cpp
+++ Client.cpp
@@ -1,113 +1,130 @@
 #include "Client.h"
 #include <sstream>
 
-
 //!
 //! @brief Worker function for threaded work
-//! Randomly selects one of four modes (...)
-void Client::client(Bank &bank_ref, const std::string &name, std::vector<std::string> &reports, std::mutex &report_mutex,
-                    std::condition_variable &cv)
+//! Randomly selects one of four modes simulating an client action
+//! deposit, withdraw, transfer and check balance
+//! Each action catches potential error
+//! All actions are logged to a report
+void Client::simulated_client(Bank &bank_ref, const std::string &client_name, 
+                              std::vector<std::string> &reports, 
+                              std::mutex &report_mutex,
+                              std::condition_variable &report_available)
 {
     using clock = std::chrono::system_clock;
-    
+
     std::ostringstream stream;
     std::shared_ptr<BankAccount> account_ref = bank_ref.getRandomAccount();
 
     if (account_ref == nullptr)
     {
 
-        stream << " Account does not exist. Transaction attempt " << getCurrentTime() << "\n";
+        stream << " Account does not exist. Transaction attempt " << get_current_time() << "\n";
         {
             std::lock_guard<std::mutex> report_lock(report_mutex);
             reports.emplace_back(stream.str());
         }
-        cv.notify_one();
+        report_available.notify_one();
         return;
     }
+    else
     {
-        // get random value for client action
-        int randomValue{Random::get_random(0, 3)};
-        switch (randomValue)
+        // Generate a random value to simulate different client actions
+        switch (Random::get_random(0, 3))
         {
         case 0:
         {
-            int randomAmount{Random::get_random(1, 100)};
-            account_ref->deposit(randomAmount);
-            stream << name << "deposited " << randomAmount << " kr into account " << account_ref->getAccountNumber()
-                   << ", " << getCurrentTime() << "\n";
+            int random_amount{Random::get_random(1, 100)};
+            try
+            {
+                account_ref->deposit(random_amount);
+            }
+            catch (const std::invalid_argument& e)
+            {
+                stream <<"Failed to Deposit. Error: "<< e.what() << "\n";
+                {
+                    std::lock_guard<std::mutex> report_lock(report_mutex);
+                    reports.emplace_back(stream.str());
+                }
+                report_available.notify_one();
+                break;
+            }
+
+            stream << client_name << "deposited " << random_amount << " kr into account " 
+                << account_ref->getAccountNumber() << ", " << get_current_time() << "\n";
             {
                 std::lock_guard<std::mutex> report_lock(report_mutex);
                 reports.emplace_back(stream.str());
             }
-            cv.notify_one();
+            report_available.notify_one();
             break;
         }
         case 1:
         {
-            int randomAmount{Random::get_random(1, 100)};
-            if (account_ref->getBalance() >= randomAmount)
+            int random_amount{Random::get_random(1, 100)};
+            try
             {
-                account_ref->withdraw(randomAmount);
-                stream << name << "withdrew " << randomAmount << " kr from account " << account_ref->getAccountNumber()
-                       << ", " << getCurrentTime() << "\n";
+                account_ref->getBalance() >= random_amount;
+                account_ref->withdraw(random_amount);
+            }
+            catch (std::exception e)
+            {
+                stream <<"Failed to withdraw. Error: "<< e.what() << "\n";
                 {
                     std::lock_guard<std::mutex> report_lock(report_mutex);
                     reports.emplace_back(stream.str());
                 }
-                cv.notify_one();
+                report_available.notify_one();
             }
-            else
-            {
-                stream << "Insufficient funds. Transaction attempt " << getCurrentTime() << "\n";
+            stream << client_name << "withdrew " << random_amount << " kr from account " << account_ref->getAccountNumber()
+                       << ", " << get_current_time() << "\n";
                 {
                     std::lock_guard<std::mutex> report_lock(report_mutex);
                     reports.emplace_back(stream.str());
                 }
-                cv.notify_one();
-            }
+                report_available.notify_one();
             break;
         }
         case 2:
         {
-            stream << name << "checked balance in account: " << account_ref->getAccountNumber() << ". Balance: " << account_ref->getBalance()
-                   << ", " << getCurrentTime() << "\n";
+            stream << client_name << "checked balance in account: " << account_ref->getAccountNumber() << ". Balance: " << account_ref->getBalance()
+                   << ", " << get_current_time() << "\n";
             {
                 std::lock_guard<std::mutex> report_lock(report_mutex);
                 reports.emplace_back(stream.str());
             }
-            cv.notify_one();
+            report_available.notify_one();
             break;
         }
         case 3:
         {
-            std::shared_ptr<BankAccount> otherAccount = bank_ref.getRandomAccount();
-            int randomAmount{Random::get_random(1, 100)};
+            std::shared_ptr<BankAccount> other_account = bank_ref.getRandomAccount();
+            int random_amount{Random::get_random(1, 100)};
 
-            if (account_ref->transfer(randomAmount, otherAccount) != -1)
+            try
             {
-                stream << name << "transfered " << randomAmount << " kr from account " << account_ref->getAccountNumber()
-                       << " to account " << otherAccount->getAccountNumber() << ", " << getCurrentTime() << ".\n";
+                account_ref->transfer(random_amount, other_account);
+            }
+            catch (std::exception e)
+            {
+                stream << client_name << ": transfer failed: Error: "<< e.what() << "\n";
                 std::lock_guard<std::mutex> report_lock(report_mutex);
                 reports.emplace_back(stream.str());
             }
-            else
-            {
-                stream << name << ": transfer failed.\n";
+            stream << client_name << "transfered " << random_amount << " kr from account " << account_ref->getAccountNumber()
+                       << " to account " << other_account->getAccountNumber() << ", " << get_current_time() << ".\n";
                 std::lock_guard<std::mutex> report_lock(report_mutex);
                 reports.emplace_back(stream.str());
-            }
             break;
         }
         default:
             break;
         }
-
-        return;
     }
 }
 
-
-std::string getCurrentTime()
+std::string get_current_time()
 {
     std::lock_guard<std::mutex> time_mtx(ctime_mutex);
     std::time_t t = std::time(nullptr);
